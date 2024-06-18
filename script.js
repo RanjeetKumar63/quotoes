3d8b17499497fffefc48ab19de37d1e0aa1ba820
Ranjeet Kumar <143226519+RanjeetKumar63@users.noreply.github.com>
2024-06-19 00:48:38 +0530

    mmee

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yes:)   Krishna:  love you sri 😘";
+  question.innerHTML = "Yes:) love you sri 😘";
 });
 
 noBtn.addEventListener("mouseover", () => {
